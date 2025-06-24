c597bd731e64afa9d53d1238ebe4ac8077b5d169
Great <edosegheosamagbe@gmail.com>
2025-06-24 18:31:55 +0100

    Skills section complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,15 @@ import getVerification from '../../public/get-verification.png';
 import mintProof from '../../public/mint-proof.png';
 import next from '../../public/next.png';
 
+import walletLogin from '../../public/login.png';
+import clVeri from '../../public/cl-verification.png';
+import globalPort from '../../public/global-port.png';
+import instaMint from '../../public/inst-mint.png';
+import easyInt from '../../public/easy-int.png';
+import pubSkiGallery from '../../public/pub-skill.png';
+
+import chainWallet from '../../public/chain-wallet.png';
+
 const Home = () => {
   return (
     <main>
@@ -91,6 +100,211 @@ const Home = () => {
                 permanently stored on blockchain.
               </span>
             </p>
+          </span>
+        </div>
+      </section>
+
+      {/* FEATURES SECTION */}
+      <section className="mt-30 bg-[#F1F1F1] p-10">
+        <h1 className="text-center font-manrope font-bold text-3xl mb-5">
+          {' '}
+          SkillProof Features{' '}
+        </h1>
+        <p className="text-center w-[512px] mx-auto mb-5">
+          Everything you need to build and showcase your professional reputation
+          on-chain.
+        </p>
+
+        <div className="flex gap-5 flex-wrap">
+          <span className="flex flex-col justify-center h-[356px] w-[calc(33.333%-1.25rem)] bg-white p-10 gap-4 ">
+            <Image src={walletLogin} alt="" />
+            <p className="font-bold font-manrope text-xl">Wallet-Based Login</p>
+            <p className="font-medium font-manrope">
+              Connect with MetaMask or any wallet to verify and own your work—no
+              passwords needed.
+            </p>
+          </span>
+
+          <span className="flex flex-col justify-center h-[356px] w-[calc(33.333%-1.25rem)] bg-white p-10 gap-4">
+            <Image src={clVeri} alt="" />
+            <p className="font-bold font-manrope text-xl">
+              Client Verification
+            </p>
+            <p className="font-medium font-manrope">
+              Secure digital signatures from clients provide authentic proof of
+              work completion.
+            </p>
+          </span>
+
+          <span className="flex flex-col justify-center h-[356px] w-[calc(33.333%-1.25rem)] bg-white p-10 gap-4">
+            <Image src={globalPort} alt="" />
+            <p className="font-bold font-manrope text-xl">Global Portfolio</p>
+            <p className="font-medium font-manrope">
+              Showcase your verified work history to client worldwide with
+              blockchain-backed proof.
+            </p>
+          </span>
+
+          <span className="flex flex-col justify-center h-[356px] w-[calc(33.333%-1.25rem)] bg-white p-10 gap-4">
+            <Image src={instaMint} alt="" />
+            <p className="font-bold font-manrope text-xl"> Instant Minting </p>
+            <p className="font-medium font-manrope">
+              Automated NFT creation upon client verification with low gas fees
+              and fast processing.
+            </p>
+          </span>
+
+          <span className="flex flex-col justify-center h-[356px] w-[calc(33.333%-1.25rem)] bg-white p-10 gap-4">
+            <Image src={easyInt} alt="" />
+            <p className="font-bold font-manrope text-xl"> Easy Integration </p>
+            <p className="font-medium font-manrope">
+              Simple tools for client to verify work without needing deep web3
+              knowledge.
+            </p>
+          </span>
+
+          <span className="flex flex-col justify-center h-[356px] w-[calc(33.333%-1.25rem)] bg-white p-10 gap-4">
+            <Image src={pubSkiGallery} alt="" />
+            <p className="font-bold font-manrope text-xl">
+              {' '}
+              Public Skill Gallery{' '}
+            </p>
+            <p className="font-medium font-manrope">
+              Explore verified work from real freelancers. Browse Skill NFTs
+              tied to real contributions.
+            </p>
+          </span>
+        </div>
+      </section>
+
+      {/* YOUR SKILLS SECTION */}
+      <section className="mt-30 p-10">
+        <div className="flex justify-between items-center mb-10">
+          <p className="font-manrope text-xl font-bold">Your Skills NFTs</p>
+
+          <span className="flex items-center gap-4">
+            <input
+              type="text"
+              placeholder="Search NFTs..."
+              className="border border-gray-200 rounded-lg py-1 px-3"
+            />
+            <button className="border border-gray-300 rounded-lg font-bold font-manrope py-1 px-3">
+              {' '}
+              All Skills{' '}
+            </button>
+          </span>
+        </div>
+
+        <div className="flex justify-between">
+          {/* SKILL ITEM */}
+          <span className="flex flex-col w-[407px] border border-gray-200 rounded-lg">
+            <Image src={chainWallet} alt="" />
+
+            <span className="p-4 flex flex-col gap-5">
+              <span className="flex items-center justify-between">
+                <p className="font-bold font-manrope">
+                  Crypto Wallet Integration
+                </p>
+                <p className="bg-[#78F9C6] text-[#286442F7] py-1 px-5 rounded-lg font-semibold">
+                  Verified
+                </p>
+              </span>
+
+              <p className="mb-3">
+                Integrated multi-chain wallet functionality into client’s
+                existing platform with enhanced security.
+              </p>
+
+              <span className="flex justify-between">
+                <p className="bg-[#6C51D93D] text-[#8E35EA] py-1 px-3 rounded-lg">
+                  Web3.js
+                </p>
+                <p className="bg-[#6C51D93D] text-[#8E35EA] py-1 px-3 rounded-lg">
+                  Web3.js
+                </p>
+                <p className="bg-[#6C51D93D] text-[#8E35EA] py-1 px-3 rounded-lg">
+                  Web3.js
+                </p>
+              </span>
+              <span className="flex justify-between mb-10">
+                <p> Client: Blockchain solutions </p>
+                <p>Value: 3 ETH</p>
+              </span>
+            </span>
+          </span>
+
+          {/* SKILL ITEM */}
+          <span className="flex flex-col w-[407px] border border-gray-200 rounded-lg">
+            <Image src={chainWallet} alt="" />
+
+            <span className="p-4 flex flex-col gap-5">
+              <span className="flex items-center justify-between">
+                <p className="font-bold font-manrope">
+                  Crypto Wallet Integration
+                </p>
+                <p className="bg-[#78F9C6] text-[#286442F7] py-1 px-5 rounded-lg font-semibold">
+                  Verified
+                </p>
+              </span>
+
+              <p className="mb-3">
+                Integrated multi-chain wallet functionality into client’s
+                existing platform with enhanced security.
+              </p>
+
+              <span className="flex justify-between">
+                <p className="bg-[#6C51D93D] text-[#8E35EA] py-1 px-3 rounded-lg">
+                  Web3.js
+                </p>
+                <p className="bg-[#6C51D93D] text-[#8E35EA] py-1 px-3 rounded-lg">
+                  Web3.js
+                </p>
+                <p className="bg-[#6C51D93D] text-[#8E35EA] py-1 px-3 rounded-lg">
+                  Web3.js
+                </p>
+              </span>
+              <span className="flex justify-between mb-10">
+                <p> Client: Blockchain solutions </p>
+                <p>Value: 3 ETH</p>
+              </span>
+            </span>
+          </span>
+
+          {/* SKILL ITEM */}
+          <span className="flex flex-col w-[407px] border border-gray-200 rounded-lg">
+            <Image src={chainWallet} alt="" />
+
+            <span className="p-4 flex flex-col gap-5">
+              <span className="flex items-center justify-between">
+                <p className="font-bold font-manrope">
+                  Crypto Wallet Integration
+                </p>
+                <p className="bg-[#78F9C6] text-[#286442F7] py-1 px-5 rounded-lg font-semibold">
+                  Verified
+                </p>
+              </span>
+
+              <p className="mb-3">
+                Integrated multi-chain wallet functionality into client’s
+                existing platform with enhanced security.
+              </p>
+
+              <span className="flex justify-between">
+                <p className="bg-[#6C51D93D] text-[#8E35EA] py-1 px-3 rounded-lg">
+                  Web3.js
+                </p>
+                <p className="bg-[#6C51D93D] text-[#8E35EA] py-1 px-3 rounded-lg">
+                  Web3.js
+                </p>
+                <p className="bg-[#6C51D93D] text-[#8E35EA] py-1 px-3 rounded-lg">
+                  Web3.js
+                </p>
+              </span>
+              <span className="flex justify-between mb-10">
+                <p> Client: Blockchain solutions </p>
+                <p>Value: 3 ETH</p>
+              </span>
+            </span>
           </span>
         </div>
       </section>
